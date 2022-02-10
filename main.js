2245804ace15f4a03264d37bd2b7a07b8e91be5b
Adam Zawierucha <zawiedev@gmail.com>
2022-02-10 03:59:48 -0600

    gameplay

--- main.js
+++ main.js
@@ -16,9 +16,18 @@ function drawHand() {
  * @param {int} cardId (an integer between 1 and 52)
  * @return the corresponding image token
  */
+function cardIdToValue(cardId) {
+    return ((cardId-1) % 13) + 1;
+}
+
+/**
+ * 
+ * @param {int} cardId (an integer between 1 and 52)
+ * @return the corresponding image token
+ */
 function cardIdToToken(cardId) {
     const s = ['k','l','p','s'][Math.floor((cardId-1)/13)];
-    const r = (cardId % 13) + 1;
+    const r = cardIdToValue(cardId);
     const v = (r == 1) ? 'a' 
                 : (r == 11) ? 'j' 
                 : (r == 12) ? 'q' 
@@ -37,11 +46,82 @@ function cardIdToImage(cardId) {
     return 'cards/'+cardIdToToken(cardId)+'.png.webp'
 }
 
+const permutator = (inputArr) => {
+    let result = [];
+  
+    const permute = (arr, m = []) => {
+      if (arr.length === 0) {
+        result.push(m)
+      } else {
+        for (let i = 0; i < arr.length; i++) {
+          let curr = arr.slice();
+          let next = curr.splice(i, 1);
+          permute(curr.slice(), m.concat(next))
+       }
+     }
+   }
+  
+   permute(inputArr)
+  
+   return result;
+  }
+  
+function getSolution(cards) {
+    const ops = ["+", "-", "*", "/", "^"];
+
+    function f(a, o, b) {
+        if (o == "+") 
+            return a+b;
+        if (o == "-") 
+            return a-b;
+        if (o == "*") 
+            return a*b;
+        if (o == "/") 
+            return a/b;
+        if (o == "/") 
+            return Math.pow(a,b);
+    }
+
+    const permutations = permutator(cards);
+    for(l in permutations) { //4!
+        p = permutations[l];
+        for(i = 0; i < 4; i ++) {
+            p[i] = cardIdToValue(p[i]);
+        }
+        for(i in ops) { //5
+            for(j in ops) { //5
+                for(k in ops) {//5
+                    const op0 = ops[i];
+                    const op1 = ops[j];
+                    const op2 = ops[k];
+                    const v = f(p[0], op0, f(p[1], op1, f(p[2], op2, p[3])));
+                    const s = `${p[0]} ${op0} (${p[1]} ${op1} (${p[2]} ${op2} ${p[3]}))`
+                    if (v == 24)
+                        return s;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+
 window.onload = function() {
-    const cards = Array.from(drawHand());
+    let solution = null;
+    let cards = null;
+    while (solution == null) {
+        cards = Array.from(drawHand());
+        solution = getSolution(cards);
+    }
+    console.log(solution);
     for(i = 0; i < 4; i ++) {
         console.log("c"+i, cards[i], cardIdToToken(cards[i]));
         document.getElementById("c"+i).src = cardIdToImage(cards[i]);
+    } 
+
+    document.getElementById('sol').onclick = function() {
+        if (confirm("A solution is:\n"+solution)) 
+            location.reload;
     }
 }
 
