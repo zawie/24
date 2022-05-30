77ec23b6e418a9c0f6a83588d035556d61e8cfcf
Adam Zawierucha <zawiedev@gmail.com>
2022-05-30 13:47:10 -0700

    difficulty selection

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Define difficulty enum 
 /**
  * @return a set of four card ids (4 distinct numbers from 1-52)
  */
@@ -135,19 +136,119 @@ function getSolution(cards) {
     return solutions;
 }
 
+function getCardsAndSolution(minSolutionCount, maxSolutionCount) {
+    console.log("Generating hand with solution between ", minSolutionCount, maxSolutionCount)
+    let cards = null;
+    let solutions = [];
+    while (solutions.length < minSolutionCount || solutions.length > maxSolutionCount) {
+        cards = Array.from(drawHand());
+        solutions = getSolution(cards)
+    }
 
+    return {
+        cards: cards,
+        solutions: solutions
+    }
+}
+
+function getCardsAndSolutionByDifficulty(difficulty) {
+    let min = 1;
+    let max = 1000; 
+    switch (difficulty) {
+        case "Any":
+            min = 1
+            max = 1000
+            break;
+        case "Easy":
+            min = 51
+            max = 1000
+            break;
+        case "Medium":
+            min = 11
+            max = 50
+            break;
+        case "Hard":
+            min = 1
+            max = 10
+            break;
+        case "Extreme":
+            min = 1
+            max = 1
+            break;
+    }
+
+    return getCardsAndSolution(min, max);
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+function readDifficulty(str) {
+    switch (str) {
+        case "Easy":
+            return "Easy"
+        case "Medium":
+            return "Medium"
+        case "Hard":
+            return "Hard"
+        case "Extreme":
+            return "Extreme"
+    }
+    return "Any"
+}
 window.onload = function() {
 
+    //Define help Functionality
     document.getElementById('help').onclick = function() {
         alert("Combine the four cards using addition, subtraction, multiplication, division, and exponentiation such that it totals 24.\n(A=1, J=11, Q=12, K=13)")
     }
 
-    let cards = null;
-    let solutions = [];
-    while (solutions.length < 1) {
-        cards = Array.from(drawHand());
-        solutions = getSolution(cards)
+    //Read difficulty
+    const difficulty = readDifficulty(findGetParameter("difficulty"));
+    const diff_select = document.getElementById('difficulty');
+    console.log("Selected difficulty: "+ difficulty)
+
+    //Set Difficulty
+    diff_select.value = difficulty
+
+    function changeDifficulty() {
+        console.log("Chanigng difficulty to:"+diff_select.value)
+
+        var url = new URL(window.location.href);
+        var search_params = url.searchParams;
+
+        // new value of "id" is set to "101"
+        search_params.set('difficulty', diff_select.value);
+
+        // change the search property of the main url
+        url.search = search_params.toString();
+
+        // the new url string
+        var new_url = url.toString();
+
+        // output : http://demourl.com/path?id=101&topic=main
+        console.log("new_url:", new_url);
+
+        // Simulate an HTTP redirect:
+        window.location.replace(new_url);
     }
+
+    diff_select.addEventListener('change', changeDifficulty, false);
+
+    //Generate Cards
+    let cardsAndSolutionPair = getCardsAndSolutionByDifficulty(difficulty)
+    const cards = cardsAndSolutionPair.cards
+    const solutions = cardsAndSolutionPair.solutions
 
     for(i = 0; i < 4; i ++) {
         console.log("c"+i, cards[i], cardIdToToken(cards[i]));
@@ -158,6 +259,7 @@ window.onload = function() {
         }, 250);          
     } 
 
+    //Deefine Soluton funcitnlaity
     document.getElementById('sol').onclick = function() {
         const l = solutions.length;
         const body = solutions.join('\n')
@@ -165,5 +267,6 @@ window.onload = function() {
                     :confirm(`There are ${l} solutions:\n${body}`))
             location.reload();
     }
+
 }
 
